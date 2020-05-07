b39bde16853e6b5717ffe496c3ec40f74217c3ef
Frank Halbach <frank.halbach@visteon.com>
2020-05-07 14:27:34 +0200

    added caching example

--- ExcelDnaSampleQuerySQLServer/MyFunctions.cs
+++ ExcelDnaSampleQuerySQLServer/MyFunctions.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using ExcelDna.Integration;
 using System;
+using System.Runtime.Caching;
 
 namespace ExcelDnaSampleQuerySQLServer
 {    
@@ -11,17 +12,21 @@ namespace ExcelDnaSampleQuerySQLServer
         // this is a quick demo to show how to query dynamically with ExcelDNA & Dapper
         // I'm extracting the column names from the DapperRow, therfore the cast to IDictionary<string,object>.https://dapper-tutorial.net/knowledge-base/26932789/dapper-results-dapper-row--with-bracket-notation
         // Don't use this in production, SQL injections etc.
-
+      
 
         [ExcelFunction(Description = "Generic Function", Category = "My Functions")]
         public static object Query(
             [ExcelArgument(Name ="SQL Query", Description ="SQL Query")] string query,
-            [ExcelArgument(Name = "SQL Connection String", Description = "SQL Connection String")] string connectionString
+            [ExcelArgument(Name = "SQL Connection String", Description = "SQL Connection String")] string connectionString,
+            [ExcelArgument(Name = "optional caching in secs", Description = "enter caching time in seconds, default is 5")] int caching
+
             )
         {
+            int cachingDurationInSeconds = caching == 0 ? 5 : caching;
+
             try
             {
-                var data = DBHelper.QueryTable(query, connectionString) as IEnumerable<IDictionary<string, object>>; ;
+                var data = QueryWithCaching(query, connectionString, cachingDurationInSeconds);
                 var result = data.ToArray();
 
                 var rowsCount = result.Length;
@@ -49,10 +54,34 @@ namespace ExcelDnaSampleQuerySQLServer
             {
 
                 return ex.Message;
-            }
-            
+            }            
           
-        }       
+        }    
+        
+
+        private static IEnumerable<IDictionary<string, object>> QueryWithCaching(string query,string connectionString,int cachingDuractionInSeconds)
+        {
+            ObjectCache cache = MemoryCache.Default;
+
+           
+
+            if (!(cache[query] is IEnumerable<IDictionary<string, object>> repsonse))
+            {
+                var cachePolicy = new CacheItemPolicy();                
+
+                cachePolicy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(cachingDuractionInSeconds);
+
+                repsonse = DBHelper.QueryTable(query, connectionString) as IEnumerable<IDictionary<string, object>>;
+
+
+                cache.Set(query, repsonse, cachePolicy);
+
+            }               
+
+
+            return repsonse;
+
+        }
        
     }       
 }
